987a821ffe3b7ab249cf415d66d00f547f700279
tkqubo <tk.qubo@gmail.com>
2016-02-03 02:55:25 +0900

    chore: add sample migration

---
+++ src/main/resources/db/migration/V20160202032023462__init.sql
@@ -0,0 +1,6 @@
+create table users(
+  name VARCHAR(255) PRIMARY KEY,
+  email VARCHAR(255)
+);
+insert into users values ('sample1', 'sample1@example.com');
+insert into users values ('sample2', 'sample2@example.com');
